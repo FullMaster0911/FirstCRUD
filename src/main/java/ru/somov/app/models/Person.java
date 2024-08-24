78d3aa311071b94002a2eeb5abc079e30cf46651
zh.somov2006 <zh.somov2006@gmail.com>
2024-08-25 05:11:38 +0700

    first commit

--- src/main/java/ru/somov/app/models/Person.java
+++ src/main/java/ru/somov/app/models/Person.java
@@ -1,4 +1,64 @@
 package ru.somov.app.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class Person {
+    private int id;
+
+    @NotEmpty(message = "Empty error")
+    @Size(min = 2, max = 30, message = "Size error")
+    private String name;
+
+    @Min(value = 0, message = "Min age error")
+    private int age;
+
+    @NotEmpty(message = "Empty error")
+    @Email(message = "Email form error")
+    private String email;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
